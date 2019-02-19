a6ce804523760936cc9c24ea906d37d10ed12227
Oskar Alfaro <ogalfaro@ucsc.edu>
2019-02-18 22:10:38 -0800

    Particle System!!! Works!!

---
+++ Assignment 5/Assets/play.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class play : MonoBehaviour
+{
+    public GameObject particle;
+    public GameObject portal;
+    // Start is called before the first frame update
+    void Start()
+    { 
+        particle.GetComponent<ParticleSystem>().Stop();  
+    }
+
+    void Update(){
+        if(portal.GetComponent<GoRed>().points > 0 && !particle.GetComponent<ParticleSystem>().isPlaying){ 
+            particle.GetComponent<ParticleSystem>().Play();
+        }
+    }
+    
+}
